cb5541f178a51e9b96c785bd4a329be0f24e81ae
Ethan Raymond <ethan.raymond14@gmail.com>
2016-03-20 13:33:49 +0800

    removed /public static reference

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ var app = express();
 app.use(cors()); // add this line below it
 
 // Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+//app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
