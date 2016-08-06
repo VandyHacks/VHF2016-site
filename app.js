fbec54f809c00c39358f5548708d96e7e3f84c7f
Ethan Raymond <ethan.raymond14@gmail.com>
2016-08-06 14:08:05 -0700

    Add express for prod app

---
+++ app.js
@@ -0,0 +1,15 @@
+const path = require('path'); 
+const express = require('express');
+const webpack = require('webpack'); 
+const webpackMiddleware = require('webpack-dev-middleware');
+const config = require('./webpack.config.js');
+
+const app = express(); 
+
+app.use(express.static(__dirname)); 
+app.get('/', function response(req, res) {  
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+
+app.listen(process.env.PORT || 3000);
