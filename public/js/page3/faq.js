b4da81ec5c11eeb081ee798285db4881f91bf7af
Melvin Lu <melvinlu1996@gmail.com>
2016-08-06 18:12:32 -0400

    Add part of FAQ page

---
+++ public/js/page3/faq.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class FAQ extends React.Component {
+	render() {
+		return(
+			<div>
+			<p className="faq">FREQUENTLY ASKED QUESTIONS:</p>
+			</div>
+		);
+	}
+}
---
+++ public/js/page3/ground.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Ground3 extends React.Component {
+	render() {
+		return(
+			<div>
+			{/* <img src="public/img/city.svg" className="city"/> */}
+			</div>
+		);
+	}
+}
