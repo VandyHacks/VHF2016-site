356c7a13ae430ae02de39156e5d5f56848fcdc63
Melvin Lu <melvinlu1996@gmail.com>
2016-08-08 17:03:44 -0400

    Fix paths for page 3

--- public/js/page3/ground.js
+++ public/js/page3/ground.js
@@ -4,10 +4,10 @@ export default class Ground3 extends React.Component {
 	render() {
 		return(
 			<div>
-			<img src="public/img/city.svg" className="city"/>
-			<img src="public/img/confused_squirrel.svg" className="faqSquirrel"/>
-			<img src="public/img/trees1.svg" className="trees1"/>
-			<img src="public/img/trees2.svg" className="trees2"/>
+			<img src="img/city.svg" className="city"/>
+			<img src="img/confused_squirrel.svg" className="faqSquirrel"/>
+			<img src="img/trees1.svg" className="trees1"/>
+			<img src="img/trees2.svg" className="trees2"/>
 			</div>
 		);
 	}
