16d5c7e432d79d1680c086901d07be3d2ada3d4c
Sam Pepose <sampepose@gmail.com>
2016-08-11 22:22:01 -0500

    Added hacky jQuery to smooth-scroll horizontally

---
+++ public/js/scroll.js
@@ -0,0 +1,63 @@
+const NUM_PAGES = 3;
+const ANIMATION_TIME = 700;
+let currentPage = 0;
+
+// From underscore.js
+function throttle(func, wait, options) {
+  var context, args, result;
+  var timeout = null;
+  var previous = 0;
+  if (!options) options = {};
+  var later = function() {
+    previous = options.leading === false ? 0 : Date.now();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+  return function() {
+    var now = Date.now();
+    if (!previous && options.leading === false) previous = now;
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+};
+
+const cb = (e) => {
+  const oldPage = currentPage;
+
+  if (e.deltaY > 0 || e.deltaX < 0) { // page left
+    currentPage = Math.max(currentPage - 1, 0);
+  } else if (e.deltaY < 0 || e.deltaX > 0) { // page right
+    currentPage = Math.min(currentPage + 1, NUM_PAGES - 1);
+  }
+
+  // exit early if on an edge of the window
+  if (oldPage === currentPage) {
+    return;
+  }
+
+  $('html, body').animate({
+    scrollLeft: currentPage * $(window).width(),
+  }, ANIMATION_TIME, 'linear');
+
+  e.preventDefault();
+  return false;
+}
+
+$('html, body').on(
+  'scroll touchmove mousewheel',
+  throttle(cb, ANIMATION_TIME * 2, {trailing: false})
+);
