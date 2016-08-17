0db9372ca3fa782c0cd685c2775fa5003cae4dc2
Ethan Raymond <ethan.raymond14@gmail.com>
2016-08-16 21:35:45 -0700

    Added constants file and started FAQ

--- public/js/page3/faq.js
+++ public/js/page3/faq.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 export default class FAQ extends React.Component {
-	render() {
+
+  render() {
+
+    // for each question in the faqs array (this.props.faqs)
+    // construct the FAQ question
+    // <div>
+    //   <h3> question['question']</h3>
+    //   <p> question['answer']</p>
+    // </div>
+    // Render the questions in the return method
 		return(
 			<div>
 			<p className="faq">FREQUENTLY ASKED QUESTIONS:</p>
 			</div>
 		);
 	}
+}
+
+FAQ.propTypes = {
+  faqs: React.PropTypes.Array.isRequired,
 }
