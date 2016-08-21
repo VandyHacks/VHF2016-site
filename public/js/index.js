931f1f71ee90c66a0cea9a73af2e31154fa0212b
Melvin Lu <melvinlu1996@gmail.com>
2016-08-21 15:04:43 -0400

    Start of mobile responsiveness

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,3 @@
-// Class names of the 3 different pages that overlap should be renamed in order to
-// avoid confusion and improve readability (Ex. 'Sky1' & 'Sky2')
-
 require('font-awesome/css/font-awesome.css');
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -24,7 +21,7 @@ scroll.init();
 class Navbar extends React.Component {
   render() {
     return (
-      <div className="col-md-5 col-md-offset-5 text-center">
+      <div className="col-md-5 col-md-offset-5 text-center hidden-xs hidden-sm">
         <span className="navbar-btn" onClick={scroll.scrollToPage.bind(scroll, 0)}>Home</span>
         <span className="navbar-btn" onClick={scroll.scrollToPage.bind(scroll, 1)}>About</span>
         <span className="navbar-btn" onClick={scroll.scrollToPage.bind(scroll, 2)}>FAQ</span>
@@ -42,9 +39,9 @@ class Page1 extends React.Component {
 			<Description />
 			<div id="grass"/>
 			<Ground />
-      <i className="fa fa-fw fa-arrow-circle-o-right fa-5x"
-        id="page1-arrow-right"
-        onClick={scroll.scrollRight}></i>
+			<i className="fa fa-fw fa-arrow-circle-o-right fa-5x hidden-xs hidden-sm"
+				id="page1-arrow-right"
+				onClick={scroll.scrollRight}></i>
 			</div>
 		);
 	}
@@ -56,12 +53,12 @@ class Page2 extends React.Component {
 			<div>
 			<Sky2 />
 			<Ground2 />
-      <i className="fa fa-fw fa-arrow-circle-o-left fa-5x"
-        id="page2-arrow-left"
-        onClick={scroll.scrollLeft}></i>
-      <i className="fa fa-fw fa-arrow-circle-o-right fa-5x"
-        id="page2-arrow-right"
-        onClick={scroll.scrollRight}></i>
+			<i className="fa fa-fw fa-arrow-circle-o-left fa-5x hidden-xs hidden-sm"
+				id="page2-arrow-left"
+				onClick={scroll.scrollLeft}></i>
+			<i className="fa fa-fw fa-arrow-circle-o-right fa-5x hidden-xs hidden-sm"
+				id="page2-arrow-right"
+				onClick={scroll.scrollRight}></i>
 			</div>
 		);
 	}
@@ -74,12 +71,12 @@ class Page3 extends React.Component {
 			<FAQ />
 			<Sky3 />
 			<Ground3 />
-      <i className="fa fa-fw fa-arrow-circle-o-left fa-5x"
-        id="page3-arrow-left"
-        onClick={scroll.scrollLeft}></i>
-      <i className="fa fa-fw fa-arrow-circle-o-right fa-5x"
-        id="page3-arrow-right"
-        onClick={scroll.scrollRight}></i>
+			<i className="fa fa-fw fa-arrow-circle-o-left fa-5x hidden-xs hidden-sm"
+				id="page3-arrow-left"
+        		onClick={scroll.scrollLeft}></i>
+			<i className="fa fa-fw fa-arrow-circle-o-right fa-5x hidden-xs hidden-sm"
+        		id="page3-arrow-right"
+        		onClick={scroll.scrollRight}></i>
 			</div>
 		);
 	}
@@ -90,9 +87,9 @@ class Page4 extends React.Component {
 		return (
 			<div>
 			<Sponsors />
-      <i className="fa fa-fw fa-arrow-circle-o-left fa-5x"
-        id="page4-arrow-left"
-        onClick={scroll.scrollLeft}></i>
+      		<i className="fa fa-fw fa-arrow-circle-o-left fa-5x hidden-xs hidden-sm"
+        		id="page4-arrow-left"
+        		onClick={scroll.scrollLeft}></i>
 			</div>
 		);
 	}
