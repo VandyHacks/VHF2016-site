5efed3217645f6b0febb9a1ba427378c4a6453e4
Ethan Raymond <ethan.raymond14@gmail.com>
2016-08-22 18:06:07 -0400

    mentor blurb

--- public/js/page2/sky.js
+++ public/js/page2/sky.js
@@ -7,13 +7,8 @@ export default class Sky2 extends React.Component {
 			<div>
 			<Image path="img/cloud.svg" class="cloud hidden-xs hidden-sm"/>
 			<div className="mentor-description">
-				Founded in the spring of 2015, VandyHacks was the first collegiate
-	          	hackathon to call the rockin' city of Nashville, Tennessee home.  Now, we're
-	          	proud to bring you VandyHacks II- and it's gonna be even bigger and better. From
-	          	October 2-4, 250+ students from universities all over the nation will meet at
-	          	Vanderbilt University for a weekend of collaboration, innovation, and great food
-	          	(#southernhospitality).
-	          	We hope to see y'all there!
+        Interested in participating as a mentor?
+        Reach out to us <a href="mailto:info@vandyhacks.org">here</a>!
 			</div>
 			</div>
 		);
