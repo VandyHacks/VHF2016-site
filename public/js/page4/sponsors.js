09db7a962a25a634413aa52cfae1efdfac406f04
Ethan Raymond <ethan.raymond14@gmail.com>
2016-08-29 15:55:29 -0500

    change sponsor logo order on mobile

--- public/js/page4/sponsors.js
+++ public/js/page4/sponsors.js
@@ -6,13 +6,13 @@ export default class Sponsors extends React.Component {
 			<div>
 			<p className="sponsors">Sponsors:</p>
 			{/* Logos */}
-  			<img src="img/page4/microsoft.svg" className="logo1"/>
-  			<img src="img/page4/digital_reasoning.png" className="logo2"/>
+			<img src="img/page4/bny.svg" className="logo5" />
+      <img src="img/page4/digital_reasoning.png" className="logo2"/>
+			<img src="img/page4/capital_one.svg" className="logo6" />
 			<img src="img/page4/initial_state.png" className="logo3" />
 			<img src="img/page4/launch_tn.png" className="logo4" />
-			<img src="img/page4/bny.svg" className="logo5" />
-			<img src="img/page4/capital_one.svg" className="logo6" />
 			<img src="img/page4/thiel.png" className="logo7" />
+      <img src="img/page4/microsoft.svg" className="logo1"/>
 			<img src="img/page4/tree3.svg" className="sponsorTrees1 hidden-xs hidden-sm"/>
 			<img src="img/page4/tree4.svg" className="sponsorTrees2 hidden-xs hidden-sm"/>
 			<img src="img/page4/squirrel_and_laptop.svg" className="squirrelLaptop hidden-xs hidden-sm"/>
