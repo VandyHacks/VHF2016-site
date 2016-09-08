443a002333a63d7dda19223ee2592f01bd80ee8c
Ethan Raymond <ethan.raymond14@gmail.com>
2016-09-08 18:27:28 -0500

    and rel to reg page

--- public/js/page0/registration.js
+++ public/js/page0/registration.js
@@ -12,16 +12,16 @@ export default class Registration extends React.Component {
         <div className="register-button-container">
           <a
             href="https://vandyhacks.typeform.com/to/RqdpnL"
-            target="_blank"
+            target="_blank" rel="noopen"
             className="register-btn btn btn-primary btn-primary"
           >
           <span className="register-btn-text">Apply</span>
           </a>
         </div>
         <ul className="soc">
-          <li><a className="soc-facebook" href="https://www.facebook.com/vandyhacks" target="_blank"></a></li>
-          <li><a className="soc-twitter" href="https://twitter.com/vandyhacks" target="_blank" ></a></li>
-          <li><a className="soc-instagram" href="https://www.instagram.com/vandyhacks/" target="_blank" ></a></li>
+          <li><a className="soc-facebook" href="https://www.facebook.com/vandyhacks" target="_blank" rel="noopen"></a></li>
+          <li><a className="soc-twitter" href="https://twitter.com/vandyhacks" target="_blank" rel="noopen" ></a></li>
+          <li><a className="soc-instagram" href="https://www.instagram.com/vandyhacks/" target="_blank" rel="noopen" ></a></li>
         </ul><br />
         <Image class="tree0-1 hidden-xs hidden-sm" path ="img/page0/tree1.svg"/>
         <Image class="tree0-2 hidden-xs hidden-sm" path="img/page0/tree2.svg"/>
