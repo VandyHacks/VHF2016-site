07d3a88bef4be97c4e9e02a2e2a184ffee5ebacd
Ethan Raymond <ethan.raymond14@gmail.com>
2016-09-08 17:16:58 -0500

    meta tag and open tabs for sponsors

--- public/js/page4/sponsors.js
+++ public/js/page4/sponsors.js
@@ -5,31 +5,31 @@ export default class Sponsors extends React.Component {
 		return(
 			<div>
 			{/* Logos */}
-      <a href="https://www.bnymellon.com">
+      <a target="_blank" rel="noopener" href="https://www.bnymellon.com">
         <img src="img/page4/bny.svg" className="logo5" />
       </a>
-      <a href="http://www.centene.com/">
+      <a target="_blank" rel="noopener" href="http://www.centene.com/">
         <img src="img/page4/centene_logo.jpg" className="centene-logo" />
       </a>
-			<a href="http://www.digitalreasoning.com/">
+			<a target="_blank" rel="noopener" href="http://www.digitalreasoning.com/">
 	    	<img src="img/page4/digital_reasoning.png" className="logo2"/>
       </a>
-			<a href="https://www.capitalone.com/">
+			<a target="_blank" rel="noopener" href="https://www.capitalone.com/">
 			  <img src="img/page4/capital_one.svg" className="logo6" />
       </a>
-      <a href="https://seatgeek.com">
+      <a target="_blank" rel="noopener" href="https://seatgeek.com">
 			  <img src="img/page4/seat_geek.png" className="logo8"/>
       </a>
-			<a href="https://www.initialstate.com/">
+			<a target="_blank" rel="noopener" href="https://www.initialstate.com/">
 			  <img src="img/page4/initial_state.png" className="logo3" />
       </a>
-			<a href="http://launchtn.org/">
+			<a target="_blank" rel="noopener" href="http://launchtn.org/">
 			  <img src="img/page4/launch_tn.png" className="logo4" />
       </a>
-			<a href="http://thielfellowship.org/">
+			<a target="_blank" rel="noopener" href="http://thielfellowship.org/">
 			  <img src="img/page4/thiel.png" className="logo7" />
       </a>
-      <a href="https://www.stickermule.com/">
+      <a target="_blank" rel="noopener" href="https://www.stickermule.com/">
     		<img src="img/page4/sticker_mule.png" className="logo9"/>
       </a>
 			<img src="img/page4/tree3.svg" className="sponsorTrees1 hidden-xs hidden-sm"/>
