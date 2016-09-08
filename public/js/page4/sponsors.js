427d3eb2d80e1474b14f09fbbc7d905df53d43ec
Ethan Raymond <ethan.raymond14@gmail.com>
2016-09-08 10:11:49 -0500

    new sponsors

--- public/js/page4/sponsors.js
+++ public/js/page4/sponsors.js
@@ -6,13 +6,27 @@ export default class Sponsors extends React.Component {
 			<div>
 			<p className="sponsors">Sponsors:</p>
 			{/* Logos */}
-			<img src="img/page4/bny.svg" className="logo5" />
+      <a href="https://www.bnymellon.com">
+        <img src="img/page4/bny.svg" className="logo5" />
+      </a>
+			<a href="http://www.digitalreasoning.com/">
 	    	<img src="img/page4/digital_reasoning.png" className="logo2"/>
+      </a>
+			<a href="https://www.capitalone.com/">
 			<img src="img/page4/capital_one.svg" className="logo6" />
+      </a>
+			<a href="https://www.initialstate.com/">
 			<img src="img/page4/initial_state.png" className="logo3" />
+      </a>
+			<a href="http://launchtn.org/">
 			<img src="img/page4/launch_tn.png" className="logo4" />
+      </a>
+			<a href="http://thielfellowship.org/">
 			<img src="img/page4/thiel.png" className="logo7" />
+      </a>
+			<a href="http://www.digitalreasoning.com/">
     		<img src="img/page4/microsoft.svg" className="logo1"/>
+      </a>
 			<img src="img/page4/tree3.svg" className="sponsorTrees1 hidden-xs hidden-sm"/>
 			<img src="img/page4/tree4.svg" className="sponsorTrees2 hidden-xs hidden-sm"/>
 			<img src="img/page4/squirrel_and_laptop.svg" className="squirrelLaptop hidden-xs hidden-sm"/>
