61fe5218a304059cb8f97fcc7b69325788310f1c
Ethan Raymond <ethan.raymond14@gmail.com>
2016-09-08 16:54:17 -0500

    more link stuff

--- public/js/page1/description.js
+++ public/js/page1/description.js
@@ -12,7 +12,7 @@ export default class Description extends React.Component {
           proud to bring you VandyHacks III- and it's gonna be even bigger and better. From
           November 11-13, 350+ students from universities all over the nation will meet at
           Vanderbilt University for a weekend of collaboration, innovation, and great food
-          (<a href="https://twitter.com/vandyhacks" target="_blank">
+          (<a className="description-link" href="https://twitter.com/vandyhacks" target="_blank">
             <span id="description-hashtag">#southernhospitality</span>
           </a>).<br />
           We hope to see y'all there!
