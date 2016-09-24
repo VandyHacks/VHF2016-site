4f722639aec254dea103f712e1a3033ee8ee0d58
Melvin Lu <melvinlu1996@gmail.com>
2016-09-24 18:26:50 -0500

    Add MLH badge & beginner text

--- public/js/page1/sky.js
+++ public/js/page1/sky.js
@@ -4,7 +4,8 @@ import Cloud from './cloud.js'
 export default class Sky extends React.Component {
 	render() {
 		return (
-			<div className="hidden-xs hidden-sm">
+			<div>
+				<p className="beginner">Are you a beginner? No problem! Beginners are welcome at VandyHacks!<br /> We'll have workshops throughout the weekend to help you get started and learn new things - as well as mentors to help you along the way!</p>
 			</div>
 		);
 	};
