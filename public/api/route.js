e8ba35a20a1af04e8184a67d95e59c01466975d1
Hunter Manhart <Hunter.M.Manhart@vanderbilt.edu>
2016-10-22 11:20:17 -0400

    routes set

--- public/api/route.js
+++ public/api/route.js
@@ -1,11 +1,13 @@
+console.log("working");
+
 "use strict";
 var pg = require('pg');
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 var express = require('express');
 var app = express();
-//require('dotenv').config();
-//the above line should be uncommented when run locally and commented back again when pushed
+// require('dotenv').config();
+// the above line should be uncommented when run locally and commented back again when pushed
 var client = new pg.Client({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -14,8 +16,21 @@ var client = new pg.Client({
     host: process.env.DB_HOST,
     ssl: true
 });
-client.connect();
+//client.connect();
+//console.log(process.env);
 
-app.get('/checkemail', function(req, res){
-	console.log("GET request to check email " + req.params.email);
-})
+var port = process.env.PORT || 8080;        // set our port
+
+app.route('/checkemail')
+	.get(function(req, res){
+		console.log("GET request to check email " + req.query.email);
+		res.send(req.query);
+	});
+
+app.route('/checkin')
+	.post(function(req, res){
+		console.log("POST request to check in email " + req.query.email);
+		res.send(req.query);
+	});
+
+app.listen(port);
