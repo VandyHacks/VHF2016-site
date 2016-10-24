63f01b5b4db06913fc51fcfec4c5c6f6a2498815
Ethan Raymond <ethan.raymond14@gmail.com>
2016-10-24 18:34:53 -0500

    use pool

--- public/routes/rsvp.js
+++ public/routes/rsvp.js
@@ -1,69 +1,84 @@
-const path = require('path'); 
+const path = require('path');
 const pg = require('pg');
 const validator = require('validator');
 
-  function getDB() {
-    return new pg.Client({
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB,
-      port: 5432,
-      host: process.env.DB_HOST,
-      ssl: true
-    });
-  }
-  
-  function rsvp_get(app) {
-    app.get('/rsvp/:user_id', (req, res) => {
-      res.sendFile(path.join(__dirname, '../rsvp.html'));
-    });
-  }
-  
-  function rsvp_post(app) {
-    app.post('/rsvp/:user_id/', (req, res) => {
-      var hashed_id = req.params.user_id;
-      if (!validator.isAlphanumeric(hashed_id)) {
-        console.log('non alphanumberic hashed_id');
-        res.status(400).send('error')
-        return;
-      }
-      var client = getDB();
-      try {
-        client.connect();
-      } catch(err) {
-        console.log(err);
-      }
-      var query = 'select id from hacker where hashed_id = \'' + hashed_id + '\';';
-      console.log(query);
-      client.query(query, function(err, result) {
-        if (err || !result.rows[0]) {
-            console.log(err);
-            console.log(query);
-            res.status(400).send('error')
-            client.end();
-            return;
-        }
-        var hacker_id = result.rows[0].id;
-        if (!hacker_id) {
-          res.status(400).send('error')
-          client.end();
-          return;
-        }
-        query = 'update application set rsvp = TRUE where hackerid = \'' + hacker_id + '\'';
-        var hacker_udate = client.query(query, function(err, result) {
-            if (err) {
-                console.log(err);
-                console.log(query);
-                res.status(400).send('error')
-                client.end();
-                return;
+function getPool() {
+  var config = {
+    user: process.env.DB_USER,
+    database: process.env.DB,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: 5432,
+    max: 500, // max number of clients in the pool
+    idleTimeoutMillis: 15000,
+    ssl: true,
+  };
+  return new pg.Pool(config);
+}
+
+function rsvp_get(app) {
+  app.get('/rsvp/:user_id', (req, res) => {
+    res.sendFile(path.join(__dirname, '../rsvp.html'));
+  });
+}
+
+function rsvp_post(app) {
+  app.post('/rsvp/:user_id/', (req, res) => {
+    var hashed_id = req.params.user_id;
+    if (!validator.isAlphanumeric(hashed_id)) {
+      console.log('non alphanumberic hashed_id');
+      res.status(400).send('error')
+      return;
+    }
+    var pool = getPool();
+    var get_hacker_id_query = 'select id from hacker where hashed_id = \'' + hashed_id + '\';';
+    var update_rsvp_query = 'update application set rsvp = TRUE where hackerid = $1::int;';
+    var hackerid = null;
+    pool.connect()
+      .then(client => {
+        return client
+          .query(get_hacker_id_query)
+          .then(query_res => {
+            if (!query_res.rows[0] || !query_res.rows[0].id) {
+              console.log('couldn\'t find hacker for hashed id');
+              res.status(400).send('error');
+            }
+            hackerid = query_res.rows[0].id;
+            console.log(hackerid);
+          })
+          .catch(e => {
+            client.release()
+            res.status(400).send('error');
+            console.log(e)
+          })
+          .then(() => client)
+      })
+      .then(client => {
+        return client
+          .query(update_rsvp_query, [hackerid])
+          .then(query_res => {
+            console.log(query_res);
+            if (query_res.rowCount === 0) {
+              console.log('couldn\'t update rsvp');
+              res.status(400).send('error');
             }
             res.status(200).send();
-            client.end();
-        });
-      });
+          })
+          .catch(e => {
+            client.release()
+            res.status(400).send('error');
+            console.log(e)
+          })
+          .then(() => client)
+      })
+      .then(client => client.release())
+      .then(() => res.status(400).send())
+
+    pool.on('error', function (err, client) {
+      console.error('idle client error', err.message, err.stack)
     });
-  }
+  });
+}
 
 module.exports = {
   rsvp_get,
