a58f61f375e3df177e8b62a0b5e1d1ba9023fc3a
Ethan Raymond <ethan.raymond14@gmail.com>
2016-11-07 16:38:44 -0600

    adds cors

--- public/app.js
+++ public/app.js
@@ -1,16 +1,18 @@
-const path = require('path'); 
+const path = require('path');
 const express = require('express');
-const webpack = require('webpack'); 
+const webpack = require('webpack');
+const cors = require('cors');
 const config = require('../webpack.config.js');
 const api = require('./api/route.js');
 const rsvp = require('./routes/rsvp.js');
 const checkin = require('./routes/checkin.js');
 const bodyParser = require('body-parser');
-const app = express(); 
+const app = express();
 
 // pug used for qr code
 app.set('view engine', 'pug')
 app.set('views', 'public/views');
+app.use(cors());
 
 const oneDay = 86400000;
 const publicPath = path.resolve(__dirname);
@@ -25,7 +27,7 @@ rsvp.rsvp_get(app);
 rsvp.rsvp_post(app);
 checkin.checkin_qrcode_get(app);
 
-app.get('/', function response(req, res) {  
+app.get('/', function response(req, res) {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
