b801608bda5a9e5235ced74d5ee91621704848fd
Melvin Lu <melvinlu1996@gmail.com>
2016-11-06 21:13:47 -0600

    Add VentureStorm

--- public/js/page5/partners.js
+++ public/js/page5/partners.js
@@ -4,40 +4,43 @@ import React from 'react';
  	render() {
  		return(
  			<div>
-           	<a target="_blank" rel="noopener" href="https://www.google.com/">
-           		<img className="google-logo" src="img/page5/google.png"/>
-           	</a>
-           	<a target="_blank" rel="noopener" href="https://www.weebly.com/">
-           		<img className="weebly-logo" src="img/page5/weebly.png"/>
-           	</a>
-           	<a target="_blank" rel="noopener" href="https://www.twilio.com/">
-           		<img className="twilio-logo" src="img/page5/twilio.png"/>
-           	</a>
-           	<a target="_blank" rel="noopener" href="https://www.namecheap.com/">
-           		<img className="namecheap-logo" src="img/page5/namecheap.png"/>
-           	</a>
-           	<a target="_blank" rel="noopener" href="http://www.devpost.com/">
-           		<img className="devpost-logo" src="img/page5/devpost.svg"/>
-           	</a>
-           	<a target="_blank" rel="noopener" href="https://www.stickermule.com/">
-    				  <img src="img/page4/sticker_mule.png" className="logo9"/>
-      			</a>
-            <a target="_blank" rel="noopener" href="https://www.clerky.com/">
-              <img src="img/page5/clerky.svg" className="clerky-logo"/>
-            </a>
-            <a target="_blank" rel="noopener" href="https://microsoft.com/">
-              <img src="img/page5/microsoft.png" className="microsoft-logo"/>
-            </a>
-      			<a target="_blank" rel="noopener" href="https://mlh.io/">
-    				  <img src="img/page4/mlh-logo-color.svg" className="mlh-logo"/>
-      			</a>
-           	<img className="p5tree1 hidden-xs hidden-sm" src="img/page5/tree1.svg"/>
-           	<img className="p5tree2 hidden-xs hidden-sm" src="img/page5/tree2.svg"/>
-           	<img className="p5tree3 hidden-xs hidden-sm" src="img/page5/tree3.svg"/>
-           	<img className="p5trees1 hidden-xs hidden-sm" src="img/page5/trees1.svg"/>
-           	 <p className="sponsors">Interested in sponsoring VandyHacks?<br />Reach out to us at<br />
-             	<a href="mailto:sponsor@vandyhacks.org">sponsors@vandyhacks.org</a>
-           	</p>
+			<a target="_blank" rel="noopener" href="https://www.google.com/">
+				<img className="google-logo" src="img/page5/google.png"/>
+			</a>
+			<a target="_blank" rel="noopener" href="https://www.weebly.com/">
+				<img className="weebly-logo" src="img/page5/weebly.png"/>
+			</a>
+			<a target="_blank" rel="noopener" href="https://www.twilio.com/">
+				<img className="twilio-logo" src="img/page5/twilio.png"/>
+			</a>
+			<a target="_blank" rel="noopener" href="https://www.namecheap.com/">
+				<img className="namecheap-logo" src="img/page5/namecheap.png"/>
+			</a>
+			<a target="_blank" rel="noopener" href="http://www.devpost.com/">
+				<img className="devpost-logo" src="img/page5/devpost.svg"/>
+			</a>
+			<a target="_blank" rel="noopener" href="https://www.stickermule.com/">
+				  <img src="img/page4/sticker_mule.png" className="logo9"/>
+				</a>
+			<a target="_blank" rel="noopener" href="https://www.clerky.com/">
+			<img src="img/page5/clerky.svg" className="clerky-logo"/>
+			</a>
+			<a target="_blank" rel="noopener" href="https://microsoft.com/">
+			<img src="img/page5/microsoft.png" className="microsoft-logo"/>
+			</a>
+			<a target="_blank" rel="noopener" href="http://www.venturestorm.com/">
+			<img src="img/page5/venture.png" className="venture-logo"/>
+			</a>
+			<a target="_blank" rel="noopener" href="https://mlh.io/">
+			<img src="img/page4/mlh-logo-color.svg" className="mlh-logo"/>
+			</a>
+			<img className="p5tree1 hidden-xs hidden-sm" src="img/page5/tree1.svg"/>
+			<img className="p5tree2 hidden-xs hidden-sm" src="img/page5/tree2.svg"/>
+			<img className="p5tree3 hidden-xs hidden-sm" src="img/page5/tree3.svg"/>
+			<img className="p5trees1 hidden-xs hidden-sm" src="img/page5/trees1.svg"/>
+			<p className="sponsors">Interested in sponsoring VandyHacks?<br />Reach out to us at<br />
+			<a href="mailto:sponsor@vandyhacks.org">sponsors@vandyhacks.org</a>
+			</p>
  			</div>
  		);
  	}
