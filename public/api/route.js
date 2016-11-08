d316b0f37a3f2c99461c7602fa4300a5e1060aaa
Ethan Raymond <ethan.raymond14@gmail.com>
2016-11-08 17:55:04 -0600

    change to use pg pool

--- public/api/route.js
+++ public/api/route.js
@@ -6,69 +6,105 @@ var router = express.Router()
 // the above line should be uncommented when run locally and commented back again when pushed
 //console.log(process.env.DB);
 
-var client = new pg.Client({
+var pool = getPool();
+
+function getPool() {
+  var config = {
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
     database: process.env.DB,
-    port: 5432,
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    ssl: true
-});
-client.connect(); //pg connect is better?
+    port: 5432,
+    max: 500, // max number of clients in the pool
+    idleTimeoutMillis: 15000,
+    ssl: true,
+  };
+  return new pg.Pool(config);
+}
 
 
 // checkin api calls
 router.get('/checkemail', function(req, res){
-		var email = req.query.email.toLowerCase();
-		console.log("GET request to check email " + email);
+	var email = req.query.email.toLowerCase();
+	console.log("GET request to check email " + email);
 
-		// returns t/f for if email is in table     (could select applicant name for display)
-		var query_check = "select hacker.first_name, hacker.last_name, hacker.phone_number, hacker.school, hacker.shirt_size, hacker.food_restriction, application.accepted, application.rsvp, application.attended from hacker, application where hacker.email='" + email + "' and hacker.id = application.hackerid";	 	// insecure?
+	// returns t/f for if email is in table     (could select applicant name for display)
+	var query_check = "select hacker.first_name, hacker.last_name, hacker.phone_number, hacker.school, hacker.shirt_size, hacker.food_restriction, application.accepted, application.rsvp, application.attended from hacker, application where hacker.email='" + email + "' and hacker.id = application.hackerid";	 	// insecure?
+  pool.connect().
+    then(client => {
+      return client
+        .query(query_check)
+        .then(query_res => {
+          if (query_res.rowCount === 0) {
+            console.log('couldn\'t find email');
+        	  res.status(404).send({success: false, message: "Email " + email + " not found in database"});
+          }
+          else {
+            var hacker = query_res.rows[0];
+            var name = hacker.first_name + " " + hacker.last_name;
+            var reply = {};	              	               	
 
-		client.query(query_check, function(err, result) {
-	        if (err) {
-	            console.log(err); console.log(query_check);
-	            res.status(400).send({success: false, message: "Error: " + err});
-	            return;
-	        }	        
-	        // console.log(result);
-	        if (!result.rows[0]){	        	
-	        	res.status(404).send({success: false, message: "Email " + email + " not found in database"});
-	        	return;
-	        }
-	        
-	        var hacker = result.rows[0];
-	        var name = hacker.first_name + " " + hacker.last_name;
-	        var reply = {};	              	               	
-
-        	if (!result.rows[0].accepted)
-	        	reply = {success: false, message: "Hacker " + name + " wasn't accepted"};		// accepted could be null i.e. no decision
-	        else if (!result.rows[0].rsvp)
-	        	reply = {success: false, message: "Hacker " + name + " didn't rsvp"};
-	        else
-	        	reply = {success: true, message: "Welcome " + name, name: name, school: hacker.school, tshirt_size: hacker.shirt_size, dietary_restrictions: hacker.food_restriction, phone_valid: checkPhone(hacker.phone_number), phone_number: hacker.phone_number};
-
-	        res.status(200).send(reply);	        
-      	});      	
-	});
+            if (!query_res.rows[0].accepted) {
+            	reply = {success: false, message: "Hacker " + name + " wasn't accepted"};
+            }
+              // accepted could be null i.e. no decision
+            else if (!query_res.rows[0].rsvp) {
+            	reply = {success: false, message: "Hacker " + name + " didn't rsvp"};
+            }
+            else {
+            	reply = {
+                success: true,
+                message: "Welcome " + name,
+                name: name,
+                school: hacker.school,
+                tshirt_size: hacker.shirt_size,
+                dietary_restrictions: hacker.food_restriction,
+                phone_valid: checkPhone(hacker.phone_number),
+                phone_number: hacker.phone_number
+              };
+            }
+            res.status(200).send(reply);	
+          }
+        })
+        .catch(e => {
+          client.release()
+          res.status(400).send({success: false, message: "Error: " + err});
+          console.log(e)
+        })
+        .then(() => client)
+    })
+    .then(client => client.release())
+    .then(() => res.status(400).send())
+});
 
 router.post('/checkin', function(req, res){
-		var email = req.body.email.toLowerCase();
-		console.log("POST request to checkin email " + email);
-		
-		var query_update = "update application set attended = true where id in (select application.id from hacker, application where hacker.email='" + email + "' and hacker.id = application.hackerid)";
+	var email = req.body.email && req.body.email.toLowerCase();
+	console.log("POST request to checkin email " + email);
+	
+	var query_update = "update application set attended = true where id in (select application.id from hacker, application where hacker.email='" + email + "' and hacker.id = application.hackerid)";
 
-		client.query(query_update, function(err, result) {
-	        if (err) {
-	            console.log(err); console.log(query);
-	            res.status(400).send(false);
-	            return;
-	        }
-	        console.log(result);	            
-
-	        res.status(200).send(true);	        
-      	});      	
-	});
+  pool.connect().
+    then(client => {
+      return client
+        .query(query_update)
+        .then(query_res => {
+          if (query_res.rowCount === 0) {
+            console.log('no update');
+            res.status(200).send(false);	
+          }
+          else {
+            res.status(200).send(true);	
+          }
+        })
+        .catch(e => {
+          client.release()
+          console.log(err); console.log(query);
+          res.status(400).send(false);
+        })
+        .then(() => client)
+    })
+    .then(client => client.release())
+});
 
 module.exports = router;
 
