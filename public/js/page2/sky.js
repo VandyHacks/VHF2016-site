5d5645f58444d6d80c30679d51b8fe7245a6f77c
Ethan Raymond <ethan.raymond14@gmail.com>
2016-11-09 15:57:15 -0600

    speaker page done

--- public/js/page2/sky.js
+++ public/js/page2/sky.js
@@ -5,32 +5,32 @@ export default class Sky2 extends React.Component {
 	render() {
 		return(
 			<div>
-                        <div className="speakers">Speakers</div>
+      <div className="speakers">Speakers</div>
 			<div className="jeff-description">
-                                <h5 className='speaker-header'>Jeff Rothschild</h5>
-                                <p className="speaker-description">
-                                Jeff Rothschild joined Facebook in 2005 as one of the earliest and most experienced
-                                executives. He helped the social networking site scale as its vice president of
-                                infrastructure engineering for five years, before stepping back into an advisory role in
-                                2010. He left Facebook in June 2015. Before Facebook, Rothschild, a Vanderbilt University
-                                School of Engineering alumnus, started out developing network storage technologies at
-                                Honeywell and Intel. He also cofounded several of his own Silicon Valley firms, the largest
-                                of which, Veritas Software, was sold to Symantec for $13.5 billion in stock in 2004.
-                                Rothschild is now focused on technology investing and serves on the boards of startups
-                                Primary Data, Interana and Lytmus. A trustee of Vanderbilt University, he became vice
-                                chairman elect of the board in 2016.
-                                </p>
+        <h5 className='speaker-header'>Jeff Rothschild</h5>
+        <p className="speaker-description">
+        Jeff Rothschild joined Facebook in 2005 as one of the earliest and most experienced
+        executives. He helped the social networking site scale as its vice president of
+        infrastructure engineering for five years, before stepping back into an advisory role in
+        2010. He left Facebook in June 2015. Before Facebook, Rothschild, a Vanderbilt University
+        School of Engineering alumnus, started out developing network storage technologies at
+        Honeywell and Intel. He also cofounded several of his own Silicon Valley firms, the largest
+        of which, Veritas Software, was sold to Symantec for $13.5 billion in stock in 2004.
+        Rothschild is now focused on technology investing and serves on the boards of startups
+        Primary Data, Interana and Lytmus. A trustee of Vanderbilt University, he became vice
+        chairman elect of the board in 2016.
+        </p>
 			</div>
 			<div className="microstrat-description">
-                                <h5 className='speaker-header'>MicroStrat CEO</h5>
-                                <p className="speaker-description">
-                                Timothy E. Lang has served as Senior Executive Vice President & Chief Technology Officer 
-                                since November 2014 and previously served as Senior Vice President, Analytics, a position he 
-                                had held since September 2014. Prior to joining MicroStrategy, Mr. Lang served as the chief product officer
-                                for Talemetry Inc., a leading provider of talent generation SaaS solutions, from September 2010 to September 2014,
-                                and as vice president, product for SAP SE from January 2008 to September 2010. Prior to that, Mr. Lang held positions
-                                at SAP BusinessObjects and Crystal Decisions. Mr. Lang received a B.Soc.Sci. in Information Management from the University of Melbourne.
-                                </p>
+        <h5 className='speaker-header'>Timothy Lang</h5>
+        <p className="speaker-description">
+        Timothy E. Lang has served as Senior Executive Vice President & Chief Technology Officer
+        since November 2014 and previously served as Senior Vice President, Analytics, a position he
+        had held since September 2014. Prior to joining MicroStrategy, Mr. Lang served as the chief product officer
+        for Talemetry Inc., a leading provider of talent generation SaaS solutions, from September 2010 to September 2014,
+        and as vice president, product for SAP SE from January 2008 to September 2010. Prior to that, Mr. Lang held positions
+        at SAP BusinessObjects and Crystal Decisions. Mr. Lang received a B.Soc.Sci. in Information Management from the University of Melbourne.
+        </p>
 			</div>
 			</div>
 		);
